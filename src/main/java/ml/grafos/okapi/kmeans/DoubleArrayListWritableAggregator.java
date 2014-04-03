b270f3b002f1114f18d1ad8f007ac6f7235d311b
vasia <vasilikikalavri@gmail.com>
2014-04-03 19:54:51 +0200

    aggregator rewrite without reading dimensions and simple test

--- src/main/java/ml/grafos/okapi/kmeans/DoubleArrayListWritableAggregator.java
+++ src/main/java/ml/grafos/okapi/kmeans/DoubleArrayListWritableAggregator.java
@@ -1,62 +1,35 @@
 package ml.grafos.okapi.kmeans;
 
 import ml.grafos.okapi.common.data.DoubleArrayListWritable;
+import org.apache.giraph.aggregators.BasicAggregator;
 
-import org.apache.giraph.aggregators.Aggregator;
-import org.apache.giraph.conf.DefaultImmutableClassesGiraphConfigurable;
-import org.apache.hadoop.io.DoubleWritable;
+public class DoubleArrayListWritableAggregator extends BasicAggregator<DoubleArrayListWritable> {
 
-public class DoubleArrayListWritableAggregator extends DefaultImmutableClassesGiraphConfigurable 
-	implements Aggregator<DoubleArrayListWritable> {
-	
-	private int dimensions; 
-	private DoubleArrayListWritable value;
-
-	public DoubleArrayListWritableAggregator() {
-		value = createInitialValue();
-	}
-	
 	@Override
 	public void aggregate(DoubleArrayListWritable other) {
-		for ( int i = 0; i < dimensions; i++ ) {
-			value.set(i, new DoubleWritable(value.get(i).get() + other.get(i).get()));
+		DoubleArrayListWritable value = getAggregatedValue();
+		if ( value.size() == 0 ) {
+			// first-time creation
+			for ( int i = 0; i < other.size(); i ++ ) {
+				value.add(other.get(i));
+			}
+			setAggregatedValue(value);
+		}
+		else if ( getAggregatedValue().size() < other.size() ) {
+			throw new IndexOutOfBoundsException("The value to be aggregated " +
+					"cannot have larger size than the aggregator value");
+		}
+		else {
+			for ( int i = 0; i < other.size(); i ++ ) {
+				value.get(i).set(value.get(i).get() + other.get(i).get());
+			}
 		}
 		
 	}
 
 	@Override
 	public DoubleArrayListWritable createInitialValue() {
-		dimensions = getConf().getInt("kmeans.points.dimensions", 0);
-		if ( dimensions > 0 ) {
-			DoubleArrayListWritable toReturn = new DoubleArrayListWritable();
-			for ( int i=0; i < dimensions; i++ ) {
-				toReturn.add(new DoubleWritable(0));
-			}
-			return toReturn;
-		}
-		else 
-			throw new IllegalArgumentException
-			("The size and dimensions of the cluster centers should be greater than zero");
+		return new DoubleArrayListWritable();
 	}
 	
-
-	@Override
-	public DoubleArrayListWritable getAggregatedValue() {
-		return value;
-	}
-
-	@Override
-	public void setAggregatedValue(DoubleArrayListWritable value) {
-		this.value = value;
-		
-	}
-
-	@Override
-	public void reset() {
-		value = createInitialValue();
-		
-	}
-
-
-
 }
