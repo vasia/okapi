d68d66df5aba766ea00cdd997f8759005add378e
vasia <vasilikikalavri@gmail.com>
2014-04-03 19:11:29 +0200

    aggregator for cluster center coordinates

--- src/main/java/ml/grafos/okapi/kmeans/DoubleArrayListWritableAggregator.java
+++ src/main/java/ml/grafos/okapi/kmeans/DoubleArrayListWritableAggregator.java
@@ -3,36 +3,57 @@ package ml.grafos.okapi.kmeans;
 import ml.grafos.okapi.common.data.DoubleArrayListWritable;
 
 import org.apache.giraph.aggregators.Aggregator;
+import org.apache.giraph.conf.DefaultImmutableClassesGiraphConfigurable;
+import org.apache.hadoop.io.DoubleWritable;
 
-public class DoubleArrayListWritableAggregator implements Aggregator<DoubleArrayListWritable> {
+public class DoubleArrayListWritableAggregator extends DefaultImmutableClassesGiraphConfigurable 
+	implements Aggregator<DoubleArrayListWritable> {
+	
+	private int dimensions; 
+	private DoubleArrayListWritable value;
 
+	public DoubleArrayListWritableAggregator() {
+		value = createInitialValue();
+	}
+	
 	@Override
-	public void aggregate(DoubleArrayListWritable value) {
-		// TODO Auto-generated method stub
+	public void aggregate(DoubleArrayListWritable other) {
+		for ( int i = 0; i < dimensions; i++ ) {
+			value.set(i, new DoubleWritable(value.get(i).get() + other.get(i).get()));
+		}
 		
 	}
 
 	@Override
 	public DoubleArrayListWritable createInitialValue() {
-		// TODO Auto-generated method stub
-		return null;
+		dimensions = getConf().getInt("kmeans.points.dimensions", 0);
+		if ( dimensions > 0 ) {
+			DoubleArrayListWritable toReturn = new DoubleArrayListWritable();
+			for ( int i=0; i < dimensions; i++ ) {
+				toReturn.add(new DoubleWritable(0));
+			}
+			return toReturn;
+		}
+		else 
+			throw new IllegalArgumentException
+			("The size and dimensions of the cluster centers should be greater than zero");
 	}
+	
 
 	@Override
 	public DoubleArrayListWritable getAggregatedValue() {
-		// TODO Auto-generated method stub
-		return null;
+		return value;
 	}
 
 	@Override
 	public void setAggregatedValue(DoubleArrayListWritable value) {
-		// TODO Auto-generated method stub
+		this.value = value;
 		
 	}
 
 	@Override
 	public void reset() {
-		// TODO Auto-generated method stub
+		value = createInitialValue();
 		
 	}
 
