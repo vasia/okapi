268cff016e6245a13138e16e4d9f132ae3575a9d
vasia <vasilikikalavri@gmail.com>
2014-10-21 16:35:20 +0200

    treemap implementation of OutEdges

---
+++ src/main/java/ml/grafos/okapi/common/edge/TreeSetOutEdges.java
@@ -0,0 +1,154 @@
+package ml.grafos.okapi.common.edge;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.util.AbstractMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeSet;
+
+import org.apache.giraph.edge.ConfigurableOutEdges;
+import org.apache.giraph.edge.Edge;
+import org.apache.giraph.edge.MapMutableEdge;
+import org.apache.giraph.edge.MutableEdge;
+import org.apache.giraph.edge.MutableOutEdges;
+import org.apache.giraph.edge.OutEdges;
+import org.apache.giraph.utils.EdgeIterables;
+import org.apache.hadoop.io.WritableComparable;
+import org.weakref.jmx.com.google.common.collect.Sets;
+
+/**
+ * {@link OutEdges} implementation backed by a {@link TreeMap}.
+ * Useful when the edges are accessed in order. 
+ *
+ * @param <I> Vertex id
+ * @param <E> Edge value
+ */
+@SuppressWarnings("rawtypes")
+public class TreeSetOutEdges<I extends WritableComparable, E extends WritableComparable>
+	extends ConfigurableOutEdges<I, E>
+	implements MutableOutEdges<I, E> {
+	
+	/** Sorted set of <trgVertexId, edgeValue> pairs */
+	private TreeSet<Map.Entry<I,E>> edgeSet;
+
+	@Override
+	public void initialize(Iterable<Edge<I, E>> edges) {
+		EdgeIterables.initialize(this, edges);
+	}
+
+	@Override
+	public void initialize(int capacity) {
+		initialize();
+		
+	}
+
+	@Override
+	public void initialize() {
+		edgeSet = Sets.newTreeSet(new EdgeValueComparator<I, E>());
+	}
+
+	/** 
+	 * Does not allow parallel edges
+	 */
+	@Override
+	public void add(Edge<I, E> edge) {
+		for (Entry<I, E> pair : edgeSet) {
+			if (pair.getKey().equals(edge.getTargetVertexId())) {
+				edgeSet.remove(pair);
+				break;
+			}
+		}
+		edgeSet.add(new AbstractMap.SimpleEntry<I, E>(edge.getTargetVertexId(), edge.getValue()));
+	}
+
+	@Override
+	public void remove(I targetVertexId) {
+		for (Entry<I, E> pair : edgeSet) {
+			if (pair.getKey().equals(targetVertexId)) {
+				edgeSet.remove(pair);
+				break;
+			}
+		}
+	}
+
+	@Override
+	public int size() {
+		return edgeSet.size();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Iterator<Edge<I, E>> iterator() {
+		return (Iterator) mutableIterator();
+	}
+
+	@Override
+	public void readFields(DataInput in) throws IOException {
+		 int numEdges = in.readInt();
+		 initialize(numEdges);
+		 for (int i = 0; i < numEdges; ++i) {
+		      I targetVertexId = getConf().createVertexId();
+		      targetVertexId.readFields(in);
+		      E edgeValue = getConf().createEdgeValue();
+		      edgeValue.readFields(in);
+		      edgeSet.add(new AbstractMap.SimpleEntry<I, E>(targetVertexId, edgeValue));
+		 }
+	}
+
+	@Override
+	public void write(DataOutput out) throws IOException {
+		 out.writeInt(edgeSet.size());
+		 for(Entry<I, E> entry : edgeSet) {
+		      entry.getKey().write(out);
+		      entry.getValue().write(out);
+		 }	
+	}
+
+	@Override
+	public Iterator<MutableEdge<I, E>> mutableIterator() {
+		return new Iterator<MutableEdge<I, E>>() {
+	      /** Wrapped set iterator. */
+	      private Iterator<Map.Entry<I, E>> setIterator =
+	          edgeSet.iterator();
+	      /** Representative edge object. */
+	      private MapMutableEdge<I, E> representativeEdge =
+	          new MapMutableEdge<I, E>();
+
+	      @Override
+	      public boolean hasNext() {
+	        return setIterator.hasNext();
+	      }
+
+	      @Override
+	      public MutableEdge<I, E> next() {
+	        representativeEdge.setEntry(setIterator.next());
+	        return representativeEdge;
+	      }
+
+	      @Override
+	      public void remove() {
+	    	  setIterator.remove();
+	      }
+	    };
+	}
+
+//	@Override
+//	public E getEdgeValue(I targetVertexId) {
+//		for (Entry<I, E> pair : edgeSet) {
+//			if (pair.getKey().equals(targetVertexId)) {
+//				return pair.getValue();
+//			}
+//		}
+//		return null;
+//	}
+//
+//	@Override
+//	public void setEdgeValue(I targetVertexId, E edgeValue) {
+//		this.remove(targetVertexId);
+//		edgeSet.add(new AbstractMap.SimpleEntry<I, E>(targetVertexId, edgeValue));
+//	}
+
+}
---
+++ src/test/java/ml/grafos/okapi/common/edge/TestTreeSetOutEdges.java
@@ -0,0 +1,116 @@
+package ml.grafos.okapi.common.edge;
+
+import static org.apache.giraph.graph.TestVertexAndEdges.instantiateOutEdges;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.giraph.edge.Edge;
+import org.apache.giraph.edge.EdgeFactory;
+import org.apache.giraph.edge.OutEdges;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
+
+@SuppressWarnings("unchecked")
+public class TestTreeSetOutEdges {
+	
+	private static final float E = 0.0001f;
+
+	  @Test
+	  public void testParallelEdges() {
+	    OutEdges<LongWritable, DoubleWritable> edges = instantiateOutEdges(TreeSetOutEdges.class);
+	
+	    // Initial edges list contains parallel edges.
+	    List<Edge<LongWritable, DoubleWritable>> initialEdges = Lists.newArrayList(
+	        EdgeFactory.create(new LongWritable(1), new DoubleWritable(1)),
+	        EdgeFactory.create(new LongWritable(2), new DoubleWritable(2)),
+	        EdgeFactory.create(new LongWritable(3), new DoubleWritable(3)),
+	        EdgeFactory.create(new LongWritable(2), new DoubleWritable(20)));
+	
+	    edges.initialize(initialEdges);
+	
+	    // Only one of the two parallel edges should be left.
+	    assertEquals(3, edges.size());
+	
+	    // Adding a parallel edge shouldn't change the number of edges.
+	    edges.add(EdgeFactory.create(new LongWritable(3), new DoubleWritable(30)));
+	    assertEquals(3, edges.size());
+	  }
+	  
+	  @Test
+	  public void testSortOrder() {
+	    OutEdges<LongWritable, DoubleWritable> edges = instantiateOutEdges(TreeSetOutEdges.class);
+
+	    List<Edge<LongWritable, DoubleWritable>> initialEdges = Lists.newArrayList(
+	        EdgeFactory.create(new LongWritable(1), new DoubleWritable(3.0)),
+	        EdgeFactory.create(new LongWritable(2), new DoubleWritable(4.5)),
+	        EdgeFactory.create(new LongWritable(3), new DoubleWritable(1.0)),
+	        EdgeFactory.create(new LongWritable(4), new DoubleWritable(-2.0)));
+	
+	    edges.initialize(initialEdges);
+	
+	    // edges should be ordered by value
+	    List<LongWritable> ids = new ArrayList<LongWritable>();
+	    for (Edge<LongWritable, DoubleWritable> edge : edges) {
+	    	ids.add(edge.getTargetVertexId());
+	    }
+	    
+	    assertEquals(4, ids.get(0).get());
+	    assertEquals(3, ids.get(1).get());
+	    assertEquals(1, ids.get(2).get());
+	    assertEquals(2, ids.get(3).get());
+	
+	  }
+	  
+	  @Test
+	  public void testGetValue() {
+	    OutEdges<LongWritable, DoubleWritable> edges = instantiateOutEdges(TreeSetOutEdges.class);
+
+	    List<Edge<LongWritable, DoubleWritable>> initialEdges = Lists.newArrayList(
+	        EdgeFactory.create(new LongWritable(1), new DoubleWritable(1)),
+	        EdgeFactory.create(new LongWritable(2), new DoubleWritable(2)),
+	        EdgeFactory.create(new LongWritable(3), new DoubleWritable(3)),
+	        EdgeFactory.create(new LongWritable(4), new DoubleWritable(4)));
+	
+	    edges.initialize(initialEdges);
+	
+	    // test edge.getValue
+	    List<DoubleWritable> values = new ArrayList<DoubleWritable>();
+	    for (Edge<LongWritable, DoubleWritable> edge : edges) {
+	    	values.add(edge.getValue());
+	    }
+	    
+	    assertEquals(1, values.get(0).get(), E);
+	    assertEquals(2, values.get(1).get(), E);
+	    assertEquals(3, values.get(2).get(), E);
+	    assertEquals(4, values.get(3).get(), E);
+	    
+	  }
+	  
+	  @Test
+	  public void testAddRemove() {
+	    OutEdges<LongWritable, DoubleWritable> edges = instantiateOutEdges(TreeSetOutEdges.class);
+
+	    List<Edge<LongWritable, DoubleWritable>> initialEdges = Lists.newArrayList(
+	        EdgeFactory.create(new LongWritable(1), new DoubleWritable(1)),
+	        EdgeFactory.create(new LongWritable(2), new DoubleWritable(2)),
+	        EdgeFactory.create(new LongWritable(3), new DoubleWritable(3)),
+	        EdgeFactory.create(new LongWritable(4), new DoubleWritable(4)));
+	
+	    edges.initialize(initialEdges);
+	    
+	    // test add
+	    edges.add(EdgeFactory.create(new LongWritable(5), new DoubleWritable(5)));
+	    assertEquals(5, edges.size());
+	
+	    // test remove
+	    LongWritable vertexID = edges.iterator().next().getTargetVertexId();
+	    edges.remove(vertexID);
+	    assertEquals(4, edges.size());
+	    	    
+	  }
+}
